9a718394984f3f0e6228bb720deee60115ab15e6
jk <joern.kreutel@beuth-hochschule.de>
2015-11-25 11:25:09 +0100

    add common lib to classpath

--- ESA/org.dieschnittstelle.jee.esa.ejbs.client/src/org/dieschnittstelle/jee/esa/ejbs/client/ejbclients/StockSystemClient.java
+++ ESA/org.dieschnittstelle.jee.esa.ejbs.client/src/org/dieschnittstelle/jee/esa/ejbs/client/ejbclients/StockSystemClient.java
@@ -8,57 +8,55 @@ import javax.naming.InitialContext;
 import org.dieschnittstelle.jee.esa.erp.ejbs.StockSystemRemote;
 import org.dieschnittstelle.jee.esa.erp.entities.IndividualisedProductItem;
 
+import org.diesch
+
 public class StockSystemClient implements StockSystemRemote {
 
 	private StockSystemRemote proxy;
 	
 	public StockSystemClient() throws Exception {
-//		Context context = new InitialContext();
-//		
-//		this.proxy = (StockSystemRemote) context
-//				.lookup("");
+		Context context = new InitialContext();
+		
+		this.proxy = (StockSystemRemote) context
+				.lookup("ejb:global/org.dieschnittstelle.jee.esa.ejbs/org.dieschnittstelle.jee.esa.shared.ejbmodule.erp/StockSystemSingleton!org.dieschnittstelle.jee.esa.erp.ejbs.StockSystemRemote");
+		
 	}
 	
 	
 	@Override
 	public void addToStock(IndividualisedProductItem product, long pointOfSaleId, int units) {
-		//this.proxy.addToStock(product, pointOfSaleId, units);
+		this.proxy.addToStock(product, pointOfSaleId, units);
 	}
 
 	@Override
 	public void removeFromStock(IndividualisedProductItem product, long pointOfSaleId,
 			int units) {
-		//this.proxy.removeFromStock(product, pointOfSaleId, units);
+		this.proxy.removeFromStock(product, pointOfSaleId, units);
 	}
 
 	@Override
 	public List<IndividualisedProductItem> getProductsOnStock(long pointOfSaleId) {
-		//return this.proxy.getProductsOnStock(pointOfSaleId);
-		return null;
+		return this.proxy.getProductsOnStock(pointOfSaleId);
 	}
 
 	@Override
 	public List<IndividualisedProductItem> getAllProductsOnStock() {
-		//return this.proxy.getAllProductsOnStock();
-		return null;
+		return this.proxy.getAllProductsOnStock();
 	}
 
 	@Override
 	public int getUnitsOnStock(IndividualisedProductItem product, long pointOfSaleId) {
-		//return this.proxy.getUnitsOnStock(product, pointOfSaleId);
-		return 0;
+		return this.proxy.getUnitsOnStock(product, pointOfSaleId);
 	}
 
 	@Override
 	public int getTotalUnitsOnStock(IndividualisedProductItem product) {
-		//return this.proxy.getTotalUnitsOnStock(product);
-		return 0;
+		return this.proxy.getTotalUnitsOnStock(product);
 	}
 
 	@Override
 	public List<Integer> getPointsOfSale(IndividualisedProductItem product) {
-		//return this.proxy.getPointsOfSale(product);
-		return null;
+		return this.proxy.getPointsOfSale(product);
 	}
 
 
